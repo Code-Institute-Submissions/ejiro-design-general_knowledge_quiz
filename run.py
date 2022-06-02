9b2a607bd1307a4fd33db2874465144293f4e5f8
ejiro-design <ajirogene55@gmail.com>
2022-06-02 21:14:36 +0000

    add dictionary to quiz

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def qanda_quiz():
+     pass
+
+def checkanswer():
+     pass
+
+def showscore():
+     pass
+
+def playagain():
+     pass
+
+
+questions_data = {
+     "What is the capital of Finland?": "Helsinki", 
+     "How many elements are there in the periodic table?": "118 elements", 
+     "What is the largest country in the world?": "Russia", 
+     "Where was the mojito cocktail created?": "Cuba" 
+}
+
+options = [
+     ["A. Tampere", "B. Turku", "C. Espoo", "D. Helsinki"],
+     ["A. 120 elements", "B. 130 elements", "C. 115 elements", "D. 118 elements"],
+     ["A. United Kingdom", "B. Russia" "C. Canada", "D. United State of America"],
+     ["A. Brazil", "B. Cuba", "D. coasta rico"]
+     ]
+
+print(questions_data)
