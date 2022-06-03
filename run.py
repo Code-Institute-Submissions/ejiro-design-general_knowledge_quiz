7463aaaca130340642cc3684b02d06e13f9c4a2e
ejiro-design <ajirogene55@gmail.com>
2022-06-03 12:20:18 +0000

    add space betwen and answer and questions

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 def qanda_quiz():
-     pass
+     """
+     defining the game variables, and using 
+     for loop to iterate over the questions and answers  
+     """
+     correct_answer = 0
+     wrong_answer = []
+     question_no = 1
+
+     for key in questions_data:
+          print(key)
+
+          for option in options:
+               print(option[0])
+# ___________________________________
 
 def checkanswer():
      pass
@@ -12,17 +25,16 @@ def playagain():
 
 
 questions_data = {
-     "What is the capital of Finland?": "Helsinki", 
-     "How many elements are there in the periodic table?": "118 elements", 
-     "What is the largest country in the world?": "Russia", 
-     "Where was the mojito cocktail created?": "Cuba" 
-}
+     "What is the capital of Finland?:" "Helsinki \n",
+     "How many elements are there in the periodic table?:" "118 elements \n",
+     "What is the largest country in the world?:" "Russia \n",
+     "Where was the mojito cocktail created?:" "Cuba \n"}
 
 options = [
-     ["A. Tampere", "B. Turku", "C. Espoo", "D. Helsinki"],
-     ["A. 120 elements", "B. 130 elements", "C. 115 elements", "D. 118 elements"],
-     ["A. United Kingdom", "B. Russia" "C. Canada", "D. United State of America"],
-     ["A. Brazil", "B. Cuba", "D. coasta rico"]
+     ["A. Tampere", "B. Turku", "C. Espoo", "D. Helsinki "],
+     ["A. 120 elements", "B. 130 elements", "C. 118 elements", "D.12 elements"],
+     ["A. United Kingdom", "B. Russia", "C. Canada", "D. United State "],
+     ["A. Brazil \n", "B. Cuba \n", "C. Coasta Rico \n", "D.Poland"] 
      ]
 
-print(questions_data)
+qanda_quiz()
