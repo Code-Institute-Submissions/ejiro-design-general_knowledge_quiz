d99999ed9fa12af500d96522f67bbbd535123d7b
ejiro-design <ajirogene55@gmail.com>
2022-06-06 15:52:14 +0000

    add an array of question prompt

--- run.py
+++ run.py
@@ -1,62 +1,19 @@
-def qanda_quiz():
-    """
-    defining the game variables, and using for 
-    loop to iterate over the questions and answers
-    """
-    right_answer = 0
-    wrong_answer = []
-    question_no = 1
+from question import questions
 
-    for key in questions_data:
-        print(key)
-        for option in options[question_no - 1]:
-            print(option)
+"""
+creating an array of question prompt for the user
+"""
 
-        choose_answer = input("enter (A,B,C or D):")
-        choose_answer = choose_answer.upper()
-        wrong_answer.append(choose_answer)
-
-        right_answer += check_answer(questions_data.get(key), choose_answer)
-        question_no += 1
-
-show_score(right_answer, choose_answer)
-# ___________________________________
-
-def check_answer(answer, choose_answer):
-
-    """
-    checking for correct or wrong answer and return a point or score
-    """
-
-    if answer == choose_answer:
-        print("correct answer:")
-        return 1
-    else:
-        print("wrong answer:")
-        return 0
-
-# ___________________________________
-def show_score(right_answer, choose_answer):
-    
-
-
-def play_again():
-    pass
-
-
-questions_data = {
-    "What is the capital of Finland?: ": "Helsinki \n",
-    "How many elements are there in the periodic table?: ": "118 elements \n",
-    "What is the largest country in the world?: ": "Russia \n",
-    "Where was the mojito cocktail created?: ": "Cuba \n"}
-
-options = [
-    ["A. Tampere", "B. Turku", "C. Espoo", "D. Helsinki \n"],
-    ["A. 120 elements", "B. 130 elements", "C. 118 elements",
-     "D. 112 elements\n"],
-    ["A. United Kingdom", "B. Russia", "C. Canada", "D. United State\n "],
-    ["A. Brazil", "B. Cuba", "C. Coasta Rico", "D. Poland\n"]
+questions_prompt = [
+    "What is the capital of Finland?:\n  A. Tampere, B. Turku, C. Espoo, D. Helsinki \n",
+    "How many elements are there in the periodic table?:\n A. 120 elements\n, B. 130 elements \n C. 112 elements\n, D. 118 elements",
+    "What is the largest country in the world?:\n  A. United Kingdom \n, B. Russia \n, C. Canada \n, D. United State\n",
+    "Where was the mojito cocktail created?:\n A. Brazil, B. Cuba, C. Coasta Rico, D. Poland\n" 
 ]
 
-qanda_quiz()
-check_answer()
+question_data = [
+    questions(questions_prompt[3], "D"),
+    questions(questions_prompt[3], "D"),
+    questions(questions_prompt[1], "B"),
+    questions(questions_prompt[1], "B"),
+]
