f71bb4d87b265526aae58e0472ea7ed398249102
ejiro-design <ajirogene55@gmail.com>
2022-06-06 22:39:19 +0000

    increment score for user in the ask question function

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ creating an array of question prompt for the user
 """
 
 questions_prompt = [
-    "What is the capital of Finland?:\n  A. Tampere, B. Turku, C. Espoo, D. Helsinki \n",
-    "How many elements are there in the periodic table?:\n A. 120 elements\n, B. 130 elements \n C. 112 elements\n, D. 118 elements",
-    "What is the largest country in the world?:\n  A. United Kingdom \n, B. Russia \n, C. Canada \n, D. United State\n",
-    "Where was the mojito cocktail created?:\n A. Brazil, B. Cuba, C. Coasta Rico, D. Poland\n" 
+    "What is the capital of Finland? \n (a) Tampere\n (b) Turku\n (c) Espoo\n (d) Helsinki\n",
+    "How many elements are there in the periodic table?:\n (a) 120 elements\n (b) 130 elements\n (c) 112 elements\n (d). 118 elements",
+    "What is the largest country in the world?:\n  (a) United Kingdom\n (b). Russia\n (c) Canada\n (d) United State\n",
+    "Where was the mojito cocktail created?:\n (a) Brazil\n (b) Cuba\n (c) Coasta Rico\n (d) Poland\n" 
 ]
 
 question_data = [
@@ -17,3 +17,19 @@ question_data = [
     questions(questions_prompt[1], "B"),
     questions(questions_prompt[1], "B"),
 ]
+
+
+def ask_question(questions_1):
+    """
+    this function is to ask the user the questions and when the user gets \n
+    the correct answer the score will increase by 1
+    """
+    score = 0
+    for question in questions_1:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("you got" " " + str(score) + "/" + str(len(questions_1)) + "correct")
+
+
+ask_question(question_data)
