600f725720f66d936f28c3e6edbf3d8e088e9ef4
ejiro-design <ajirogene55@gmail.com>
2022-06-09 00:01:38 +0000

    correct an error on the question

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ creating an array of question prompt for the user
 
 questions_prompt = [
     "What is the capital of Finland? \n (a) Tampere\n (b) Turku\n (c) Espoo\n (d) Helsinki\n",
-    "How many elements are there in the periodic table?:\n (a) 120 elements\n (b) 130 elements\n (c) 112 elements\n (d). 118 elements\n",
-    "What is the largest country in the world?:\n (a) United Kingdom\n (b). Russia\n (c) Canada\n (d) United State\n",
+    "How many elements are there in the periodic table?:\n (a) 120 elements\n (b) 130 elements\n (c) 112 elements\n (d) 118 elements\n",
+    "What is the largest country in the world?:\n (a) United Kingdom\n (b) Russia\n (c) Canada\n (d) United State\n",
     "Where was the mojito cocktail created?:\n (a) Brazil\n (b) Cuba\n (c) Coasta Rico\n (d) Poland\n" 
 ]
 
